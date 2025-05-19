a4b5da5ba2dc5499d90ca962fbf48062f89dc731
Alvvcosta <alvdri2212@gmail.com>
2025-05-19 18:45:10 -0300

    Initial commit

---
+++ Projeto Álvaro/salario/acoes.js
@@ -0,0 +1,5 @@
+const bt = document.getElementById('bt');
+bt.addEventListener('click', () => {
+  // código para mudar as imagens
+  image.src = 'https://preview.redd.it/t%C3%A1-aqui-o-contexto-v0-qox3bbmbpcvc1.jpeg?width=850&format=pjpg&auto=webp&s=fa4529ec447bacdbbeeb2a3a009a8058f60a517e';
+});
